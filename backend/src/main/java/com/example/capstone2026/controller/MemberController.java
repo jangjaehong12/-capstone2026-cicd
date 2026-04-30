b6304523b430acac770c5688b444e3c8131ed32f
장재홍 <wwg8947@g.yju.ac.kr>
2026-04-30 19:04:24 +0900

    11

--- backend/src/main/java/com/example/capstone2026/controller/MemberController.java
+++ backend/src/main/java/com/example/capstone2026/controller/MemberController.java
@@ -12,7 +12,7 @@ import java.util.Optional;
 @RestController
 @RequestMapping("/api/members")
 @RequiredArgsConstructor
-@CrossOrigin(origins = "http://localhost:63342") // 리액트 서버 포트 허용_____
+@CrossOrigin(origins = "http:/43.207.204.183:63342") // 리액트 서버 포트 허용_____
 public class MemberController {
 
     private final MemberRepository memberRepository;
